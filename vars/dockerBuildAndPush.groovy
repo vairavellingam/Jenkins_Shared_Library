20b1b2773ce0b961072bdfdfec158610b483a3f5
Vel <vairavellingam@gmail.com>
2026-05-12 15:30:53 +0530

    final commit

--- vars/dockerBuildAndPush.groovy
+++ vars/dockerBuildAndPush.groovy
@@ -1,9 +1,9 @@
-def call(String imageName, String registryCredId) {
+def call(String imageName, String dockerhub-token) {
     echo "Building Docker image ${imageName}..."
     def dockerImage = docker.build("${imageName}:latest")
     
     echo "Pushing Docker image to DockerHub..."
-    docker.withRegistry('https://registry.hub.docker.com', registryCredId) {
+    docker.withRegistry('https://registry.hub.docker.com', dockerhub-token) {
         dockerImage.push('latest')
     }
 }
--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,8 +1,8 @@
-def call(String repoUrl, String branch, String credId) {
+def call(String repoUrl, String branch, String github-token) {
     echo "Checking out code from ${repoUrl}..."
     checkout([
         $class: 'GitSCM',
         branches: [[name: branch]],
-        userRemoteConfigs: [[credentialsId: credId, url: repoUrl]]
+        userRemoteConfigs: [[credentialsId: github-token, url: repoUrl]]
     ])
 }
